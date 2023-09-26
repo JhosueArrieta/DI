1870e32e4de8201a602cbad0c2630a1af168a987
Jhosue Arrieta <jarrietac22@fpcoruna.afundacion.org>
2023-09-26 18:02:38 +0200

    Completado ejercicio 2.py

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,14 +1,71 @@
 opcion = " "
-adivinanza = "Que tiene MESSI que no tiene CR7? \n a. 5 balones de oro \n b. Un mundial \n c. Humildad  ";
-print (adivinanza)
+contCorrectas  = 0
+adivinanzas = {
+    1: "Que tiene MESSI que no tiene CR7?",
+    2: "Que tiene iago en la panza?",
+    3: "Que buga tiene victo jose?"
+} 
+opciones = {
+    1: " a. 5 balones de oro \n b. Un mundial \n c. Humildad ",
+    2: " a. Empanadillas \n b. Yogures \n c. El ombligo ",
+    3: " a. Lamborghini \n b. Fiat Multipla \n c. Peugeot 206"
+}
+respuestas = {
+    1: "b",
+    2: "a",
+    3: "c"
+}
+
+print("\n\tPrimera adivinanza")
+
+print (adivinanzas[1])
+print (opciones[1])
 opcion = input("Que opcion es correcta?")
 while (opcion != 'a') & (opcion != 'b') & (opcion != 'c'):
     opcion=input("Elige entre a/b/c ")
-if opcion == "a":
-    print ("Incorrecto")
-elif opcion == "b":
-    print ("Correcto")
+if opcion == respuestas[1] :
+    print ("Correcto, + 10 puntos")
+    contCorrectas = contCorrectas + 10
 else :
-    print ("Incorrecto")
+    print ("Incorrecto, - 5 puntos")
+    contCorrectas = contCorrectas -5 
+
+print ( "\n\tSegunda adivinanza")
+
+print (adivinanzas[2])
+print (opciones[2])
+opcion = input("Que opcion es correcta?")
+while (opcion != 'a') & (opcion != 'b') & (opcion != 'c'):
+    opcion=input("Elige entre a/b/c ")
+if opcion == respuestas[2] :
+    print ("Correcto, + 10 puntos")
+    contCorrectas = contCorrectas + 10
+else :
+    print ("Incorrecto, - 5 puntos")
+    contCorrectas = contCorrectas -5 
+
+print ( "\n\tTercera adivinanza")
+
+print (adivinanzas[3])
+print (opciones[3])
+opcion = input("Que opcion es correcta?")
+while (opcion != 'a') & (opcion != 'b') & (opcion != 'c'):
+    opcion=input("Elige entre a/b/c ")
+if opcion == respuestas[3] :
+    print ("Correcto, + 10 puntos")
+    contCorrectas = contCorrectas + 10
+else :
+    print ("Incorrecto, - 5 puntos")
+    contCorrectas = contCorrectas -5 
+##para concatenar strings con ints utilizamos str(variable numérica) como sql
+##hacer en clase o casa git add * git commit.. y git push
+print("Tu puntuación es : " + str(contCorrectas))
+
+
+
+
+
+
+
 
 
